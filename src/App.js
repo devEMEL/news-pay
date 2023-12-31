e19cb8d3eae269889288d41a0fd2d11dbc72541b
devEMEL <dominichenry882@gmail.com>
2023-12-31 22:19:31 +0100

    fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 // This file is the entry point for the app. It is used to wrap the app with the RainbowKitProvider and WagmiConfig components.
@@ -9,25 +8,24 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 // Import the connectorsForWallets function to create a list of wallets to connect to.
 // Import the RainbowKitProvider component to wrap the app with.
-import {
-  connectorsForWallets,
-  RainbowKitProvider,
-} from "@rainbow-me/rainbowkit";
-
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // Import three different wallets connectors from the RainbowKit package.
-import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
 //
-import { Chain, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 // Import configureChains, createClient, and WagmiConfig from the Wagmi package to configure the Wagmi client.
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 
 // Import the jsonRpcProvider from the Wagmi package to specify the RPC URLs of the chains we want to connect to.
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
 // Import the ToastContainer component from react-toastify to display notifications.
 import { ToastContainer } from "react-toastify";
 
 import { publicProvider } from "wagmi/providers/public";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Faucet from "./components/Faucet";
 const okpoko = {
   id: 0x500fad,
   name: "Okpoko",
@@ -67,10 +65,17 @@ function App() {
       <WagmiConfig client={wagmiClient}>
         <RainbowKitProvider chains={chains} coolMode={true}>
           <ToastContainer position={"bottom-center"} />
-          {/* header */}
-          {/* routes */}
-          {/* footer */}
-          <p className="bg-[#f3f3f3] text-2xl">hello world</p>
+          <BrowserRouter>
+            {/* header */}
+            <Header />
+            {/* routes */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/faucet" element={<Faucet />} />
+            </Routes>
+            {/* footer */}
+            <Footer />
+          </BrowserRouter>
         </RainbowKitProvider>
       </WagmiConfig>
     </div>
